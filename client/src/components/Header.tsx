d4baf7a2b077f70903ce31f19b66da63e369d798
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2022-06-11 14:48:50 +0200

    sidebar is created

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,35 +1,85 @@
-import { Link } from 'react-router-dom'
-import React from "react";
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import { useAppDispatch, useAppSelector } from "../app/hooks";
-import {useNavigate} from "react-router-dom"
-import { reset, selectAuth, logout} from "../features/auth/authSlice";
-import e from 'express';
-
+import { useNavigate } from "react-router-dom";
+import { reset, selectAuth, logout } from "../features/auth/authSlice";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
+import "./Navbar/navbar.css";
 
 const Header = () => {
-  const dispatch = useAppDispatch()
-  const navigate = useNavigate()
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
-  const {user,isError,isSuccess,isLoading,message} = useAppSelector(selectAuth)
+  const [sidebar, setSidebar] = useState<boolean>(false);
+  const showSideBar = () => setSidebar(!sidebar);
+
+  const { user } = useAppSelector(selectAuth);
+
   const onLogout = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    dispatch(logout())
-    dispatch(reset())
-    navigate("/login")
-  }
+    dispatch(logout());
+    dispatch(reset());
+    setSidebar(!sidebar)
+    navigate("/login");
+  };
 
   return (
-    <nav className="header">
-      <div className="navbar">
-        <div className="brand-nav">Hotelier</div>
-        <div className="right-nav">
-          <ul>
-            <li><Link to="/login">Login</Link> </li>
-            <li><Link to="/register">Register</Link> </li>
-            <li><button onClick={onLogout}>Logout</button>  </li>
-          </ul>
+    <>
+      <nav className="header">
+        <div className="navbar">
+          <div className="brand-nav">Hotelier</div>
+          <div className="right-nav">
+            <GiHamburgerMenu
+              className="right-nav-icon"
+              style={{ fontSize: "30px", cursor: "pointer" }}
+              onClick={showSideBar}
+            />
+            <ul className="right-nav-control">
+              {user ? (
+                <li>
+                  <button className="mybutton" onClick={onLogout}>
+                    Logout
+                  </button>
+                </li>
+              ) : (
+                <>
+                  <li>
+                    <Link to="/login">Login</Link>
+                  </li>
+                  <li>
+                    <Link to="/register">Register</Link>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+
+      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+        <ul className="nav-menu-items">
+          <li className="navbar-toggle">
+            <Link to="#">
+              <AiOutlineClose onClick={showSideBar} />
+            </Link>
+          </li>
+          {user ? (
+            <li className="nav-text">
+              <button onClick={onLogout}>Logout</button>
+            </li>
+          ) : (
+            <>
+              <li className="nav-text">
+                <Link to="/login" onClick={showSideBar}>Login</Link>
+              </li>
+              <li className="nav-text">
+                <Link to="/register" onClick={showSideBar}>Register</Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
+    </>
   );
 };
 
