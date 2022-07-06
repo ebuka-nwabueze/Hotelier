cff35c99e4b52279d9752642b997c75229902c33
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2022-07-06 10:56:49 +0200

    adjusted tsconfig and package.json to enable import

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,12 +4,18 @@ import { rootSchema } from "./graphql/schema";
 import { connectDB } from "./db/dbconfig";
 import "dotenv/config"
 import Auth from "./services/authService";
+import path from "path";
+import {fileURLToPath} from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
 
 
 
 
 const app: express.Application = express()
-const PORT: number = 8008
+const PORT: string | number = process.env.PORT || 8008
 
 connectDB()
 
@@ -29,12 +35,21 @@ app.use(
     }
 ));
 
-// app.use(
-//   '/graphql',
-//   graphqlHTTP({
-//     schema: rootSchema,
-//     graphiql: true,
-//   }));
+//serve react app during production.
+if (process.env.NODE_ENV === "production") {
+  // set build folder as static
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "../", "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    // respond with "Welcome...." when a GET request is made to the homepage
+    res.status(200).json({ message: "Welcome to Hotelier App" });
+  });
+}
+
 
 app.listen(PORT, () => {
   console.log(`server listening on ${PORT}`)
