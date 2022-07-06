58fdcbb5707fba040f5c8bd768de2998cd5d8abb
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2022-07-06 15:28:48 +0200

    static file directory changed

--- server/src/server.ts
+++ server/src/server.ts
@@ -38,7 +38,7 @@ app.use(
 //serve react app during production.
 if (process.env.NODE_ENV === "production") {
   // set build folder as static
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../../client/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.join(__dirname, "../../", "client", "build", "index.html"))
